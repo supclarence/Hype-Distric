f142ab59342abd8aff87ea35cd9b167253170276
supclarence <clarenceconde493@gmail.com>
2025-09-23 03:42:31 +0800

    Update website.js

--- website.js
+++ website.js
@@ -1,3 +1,11 @@
 function done() {
     alert("Items Added");
 }
+document.addEventListener("DOMContentLoaded", function() {
+    const popup = document.getElementById("popup");
+    const btn = document.getElementById("popup-btn");
+
+    btn.addEventListener("click", function() {
+        popup.style.display = "none";
+    });
+});
